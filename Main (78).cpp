56f08f0736d157b59931742ffffbbb88337e8fa6
samuelNavarroo <snavarro@alumno.montepiedra.edu.ec>
2025-10-19 17:59:38 -0500

    Add files via upload

---
+++ Main (78).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[high]);
+    return i + 1;
+}
+
+void quicksort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
+    int n = arr.size();
+    quicksort(arr, 0, n - 1);
+    cout << "Array ordenado: ";
+    for (int num : arr) cout << num << " ";
+    cout << endl;
+    return 0;
+}
